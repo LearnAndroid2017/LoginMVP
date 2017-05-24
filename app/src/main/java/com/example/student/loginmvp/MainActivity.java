5c16bcd78582f57d665ca080abddcec9bf13b47e
komalpathak <kpathak.dp25@gmail.com>
2017-05-24 18:45:18 +0530

    Login page update, Java file update

--- app/src/main/java/com/example/student/loginmvp/MainActivity.java
+++ app/src/main/java/com/example/student/loginmvp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.student.loginmvp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    EditText edt_usernm,edt_pass;
+    Button btn_signIn;
+    TextView txt_signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edt_usernm= (EditText) findViewById(R.id.editText);
+        edt_pass= (EditText) findViewById(R.id.editText2);
+        btn_signIn= (Button) findViewById(R.id.button2);
+        txt_signUp= (TextView) findViewById(R.id.textView2);
+
     }
+
+
 }
